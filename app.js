769eaa5e77ee680b5110decc868ed8c79c6135c1
rsbhargav <37236514+RsBhargav@users.noreply.github.com>
2021-08-31 19:24:42 +0530

    updated styles

--- app.js
+++ app.js
@@ -2,18 +2,26 @@ var btnTranslate = document.querySelector("#btn-translate")
 var txtInput = document.querySelector("#txt-input")
 var txtOutput = document.querySelector("#txt-output")
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/morse.json"
 
-function getTranslationURL(text){
-    return serverURL + "?" + "text=" + text
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
 }
 
 function clickHandler() {
-    console.log("clicked");
-    // txtOutput.innerText = "B"+txtInput.value
     var inputText = txtInput.value //taking input
     fetch(getTranslationURL(inputText))
-    .then(response => response.json())
-    .then(json => console.log(json))
-}
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            txtOutput.innerText = translatedText;
+        })
+        .catch(errorHandler)
+};
+
 btnTranslate.addEventListener("click", clickHandler)
